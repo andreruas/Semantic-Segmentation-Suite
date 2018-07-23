12cb885c2fcc9293c052041a97aa255d5b303cfe
andreruas <andreruas1@gmail.com>
2018-07-23 09:59:06 -0400

    fixed bug with combining transparent images

--- main.py
+++ main.py
@@ -86,7 +86,6 @@ num_classes = len(label_values)
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 sess=tf.Session(config=config)
-train_writer = tf.summary.FileWriter( './logs/1/train ', sess.graph)
 
 # Get the selected model.
 # Some of them require pre-trained ResNet
